94f924264d0a7fdcaa9403480104999840b0c916
TongMaker <62495807+TongMaker@users.noreply.github.com>
2026-01-24 14:19:33 +0100

    Fix 'desc' key in beverage item dictionary

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ menu = {
         {"es": "Mahou 5 Estrellas", "zh": "mahou五星啤酒", "desc": "330 ml", "price": "2,80 €", "img": ""},
         {"es": "Mahou sin alcohol", "zh": "无酒精啤酒", "desc": "330 ml", "price": "2,80 €", "img": ""},
         {"es": "Cerveza tshindao", "zh": "青岛啤酒", "desc": "330 ml", "price": "3,50 €", "img": ""},
-        {"es": "Refresco variados", "zh": "各种饮料", "cocacola, acuarius, fanta, etc": "330 ml", "price": "2,80 €", "img": ""},
+        {"es": "Refresco variados", "zh": "各种饮料", "desc": "cocacola, acuarius, fanta, etc", "price": "2,80 €", "img": ""},
         {"es": "Agua mineral", "zh": "矿泉水", "desc": "500 ml", "price": "2,50 €", "img": ""},
         {"es": "Café", "zh": "咖啡", "desc": "Delta", "price": "1,80 €", "img": ""},
         {"es": "Infusión", "zh": "茶", "desc": "Té verde", "price": "1,80 €", "img": ""},
